323b50eb948ae53cc979c9f6e0aa316afc53965b
Michael Tews <git@tews.dev>
2024-07-05 05:23:14 +0200

    update nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -220,27 +220,6 @@ else
       ft = { 'go', 'gomod' },
       build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
     },
-    {
-      'romgrk/barbar.nvim',
-      dependencies = {
-        'lewis6991/gitsigns.nvim',     -- OPTIONAL: for git status
-        'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-      },
-      init = function()
-        vim.g.barbar_auto_setup = false
-      end,
-      opts = {
-        -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-        -- animation = true,
-        -- insert_at_start = true,
-        -- …etc.
-      },
-      keys = {
-        { 'H', '<Cmd>BufferPrevious<CR>', desc = 'previous tab' },
-        { 'L', '<Cmd>BufferNext<CR>',     desc = 'next tab' },
-      },
-      version = '^1.0.0', -- optional: only update when a new 1.x version is released
-    },
     -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
     --
     -- This is often very useful to both group configuration, as well as handle
@@ -483,7 +462,8 @@ else
 
             -- Fuzzy find all the symbols in your current workspace.
             --  Similar to document symbols, except searches over your entire project.
-            map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+            map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols,
+              '[W]orkspace [S]ymbols')
 
             -- Rename the variable under your cursor.
             --  Most Language Servers support renaming across files, etc.
@@ -690,21 +670,11 @@ else
       end,
     },
 
-    { -- You can easily change to a different colorscheme.
-      -- Change the name of the colorscheme plugin below, and then
-      -- change the command in the config to whatever the name of that colorscheme is.
-      --
-      -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-      'folke/tokyonight.nvim',
-      priority = 1000, -- Make sure to load this before all the other start plugins.
+    {
+      'catppuccin/nvim',
+      priority = 1000,
       init = function()
-        -- Load the colorscheme here.
-        -- Like many other themes, this one has different styles, and you could load
-        -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-        vim.cmd.colorscheme 'tokyonight-night'
-
-        -- You can configure highlights by doing something like:
-        vim.cmd.hi 'Comment gui=none'
+        vim.cmd.colorscheme 'catppuccin'
       end,
     },
 
@@ -899,7 +869,6 @@ else
     require 'kickstart.plugins.indent_line',
     require 'kickstart.plugins.lint',
     require 'kickstart.plugins.autopairs',
-    require 'kickstart.plugins.neo-tree',
     -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
     -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
